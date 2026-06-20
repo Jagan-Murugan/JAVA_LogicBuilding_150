88136f9bedf171ba9acd1f314650b5f0dd9739d8
Jagan Murugan <your-github-jagankbd5@gmail.com>
2026-06-20 21:03:11 +0530

    Added new problems

---
+++ Last_num.java
@@ -0,0 +1,10 @@
+//Return Last Digit of Given Number
+public class Last_num {
+  public static void main(String[] args) {
+    int num = 13459;
+    
+     System.out.println("Last digit:" + num % 10);
+      
+  }
+}
+  
