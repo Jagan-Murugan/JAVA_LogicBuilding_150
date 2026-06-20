287286464a27d8e0aee7ebc6c1256a1c44116392
Jagan Murugan <your-github-jagankbd5@gmail.com>
2026-06-20 20:42:21 +0530

    Logic_build_Java

---
+++ Even.java
@@ -0,0 +1,11 @@
+public class Even{
+ public static void main(String[] args){
+  int n = 35;
+  if(n % 2 == 0){
+    System.out.print("Even");
+  }
+  else{
+    System.out.print("Odd");
+  }
+ } 
+}
