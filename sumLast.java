aaee997d4196e6ce5a14ce4508701767f2945704
Jagan Murugan <your-github-jagankbd5@gmail.com>
2026-06-20 21:13:15 +0530

    add two last digit

---
+++ sumLast.java
@@ -0,0 +1,10 @@
+public class sumLast {
+  public static void main(String[] args) {
+      int n = 3689;
+      int n1 = 34698;
+      n = n % 10;
+      n1 = n1 % 10;
+      System.err.println("Sum_last : " + (n+n1));;
+  }
+  
+}
