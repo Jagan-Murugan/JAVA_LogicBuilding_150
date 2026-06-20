ab524bfae1f0b027870accdaad3e3399b6c38f3d
Jagan Murugan <your-github-jagankbd5@gmail.com>
2026-06-20 21:22:18 +0530

    Is N an exact multiple of M

---
+++ multiple.java
@@ -0,0 +1,9 @@
+public class multiple {
+  public static void main(String[] args) {
+      int m =10 ,n = 9;
+      for(int i = 1;i <= 10;i++){
+        System.out.println(i + "x" + n + "="+(i*n));
+      } 
+  }
+  
+}
